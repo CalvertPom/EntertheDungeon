378ed671ab7edead52de2eb23165108307cd18ce
Calvert <xchaha@foxmail.com>
2018-02-22 23:45:31 +0800

    第一版的 感觉成了下一百层

--- app/src/main/java/com/graduationdesign/pxc/enterthedungeon/GameActivity.java
+++ app/src/main/java/com/graduationdesign/pxc/enterthedungeon/GameActivity.java
@@ -1,13 +1,53 @@
 package com.graduationdesign.pxc.enterthedungeon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.graduationdesign.pxc.enterthedungeon.util.OnContinueClickListener;
+import com.graduationdesign.pxc.enterthedungeon.view.GameLayout;
 
 public class GameActivity extends AppCompatActivity {
-
+    private GameLayout mGameLayout;
+    private View left;
+    private View right;
+    private View escgame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        mGameLayout = (GameLayout) findViewById(R.id.game);
+        left = findViewById(R.id.left);
+        right = findViewById(R.id.right);
+        escgame=findViewById(R.id.escgame);
+        left.setOnTouchListener(new OnContinueClickListener() {
+            @Override
+            public void handleClickEvent(View view) {
+                mGameLayout.moveLeft();
+            }
+        });
+
+
+        right.setOnTouchListener(new OnContinueClickListener() {
+            @Override
+            public void handleClickEvent(View view) {
+                mGameLayout.moveRight();
+            }
+        });
+        escgame.setOnTouchListener(new OnContinueClickListener() {
+            @Override
+            public void handleClickEvent(View view) {
+                Intent intent = new Intent(GameActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mGameLayout.stop();
+    }
+
 }
---
+++ app/src/main/java/com/graduationdesign/pxc/enterthedungeon/view/Person.java
@@ -0,0 +1,41 @@
+package com.graduationdesign.pxc.enterthedungeon.view;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+
+public class Person {
+    public static final int SPEED = 10;
+    private int mHeaderRadius;
+    private Paint mPaint;
+    //下边缘坐标
+    public int mPersonY;
+    public int mPersonX;
+    private Bitmap bitmap;
+
+
+    public Person(Paint paint, int radius, Bitmap bitmap) {
+        this.mPaint = paint;
+        this.mHeaderRadius = radius;
+        this.bitmap = bitmap;
+    }
+
+    /**
+     * 绘制一个圆形图片
+     */
+    public void draw(Canvas canvas) {
+
+        canvas.save();
+        Path path = new Path();
+        path.addCircle(mPersonX + mHeaderRadius, mPersonY + mHeaderRadius, mHeaderRadius, Path.Direction.CCW);
+
+        canvas.clipPath(path);
+        canvas.drawBitmap(bitmap, null, new RectF(mPersonX, mPersonY, mPersonX + 2 * mHeaderRadius, mPersonY + mHeaderRadius * 2), mPaint);
+
+        canvas.restore();
+    }
+
+
+}
